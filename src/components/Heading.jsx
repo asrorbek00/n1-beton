d6d634ec9fdee9ab35ba68b4b40436358ab08615
asrorbek00 <aserravshanbekov@gmail.com>
2023-12-12 14:25:28 +0500

    k

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -11,7 +11,7 @@ const Heading = () => {
     {data && data.map((info)=>{
         return <div className='heading lg:-mt-[5%] sm:-mt-9 ' >
             <div className='flex items-center'>
-            <img src={circle1} alt="" className=' lg:w-[30%] md:w-[28%] sm:w-[60%] ml-[4%] lg:mt-[1.4%] sm:-mt-[35%] lg:ml-10 md:ml-0 sm:-ml-[10%]'  />
+            <img src={circle1} alt="" className=' lg:w-[30%] md:w-[28%] sm:w-[60%] ml-[4%] lg:mt-[1.4%] sm:-mt-[12%] lg:ml-10 md:ml-0 sm:-ml-[10%]'  />
             <div className='flex flex-col sm:mt-5 '>
             <h1 className='  lg:text-7xl md:text-5xl sm:text-3xl  lg:w-4/5 md:w-4/5  sm:w-fit text-center text-white font-semibold lg:-ml-[10%] sm:-ml-[20%] number'>{info.title}</h1>
             <p className='text-center lg:text-xl sm:text-sm lg:w-[44%] md:w-[50%] lg:ml-[10%] sm:-ml-[10%]  text-[#c1c1c1] number'>{info.text}</p>
