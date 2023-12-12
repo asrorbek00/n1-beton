a437b87585293e89a523816b5912f8c7cd20dfff
asrorbek00 <aserravshanbekov@gmail.com>
2023-12-12 11:43:33 +0500

    a

--- src/pages/OurTeam.jsx
+++ src/pages/OurTeam.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useFetch } from '../hooks/useFetch'
+import circle1 from '../assets/circle1.png'
 
 const OurTeam = () => {
     const {data , isPending ,error}= useFetch('https://backend.n1beton.uz/about-us/')
@@ -7,7 +8,7 @@ const OurTeam = () => {
     <div>
         <div className='heading -mt-[5%]' >
             <div className='flex items-center'>
-            <img src="/public/circle1.png" alt="" className='w-[30%] ml-[4%]'  />
+            <img src={circle1} alt="" className='w-[30%] ml-[4%]'  />
             <div className='flex flex-col text-center w-fit  '>
             <h1 className='lg:text-7xl  text-center text-white font-semibold '>BIZNING JAMOA</h1>
             <p className=' text-[#c1c1c1] text-center'>Biz Sizning orzularingizni ro`yobga chiqarish uchun shu yerdamiz</p>
